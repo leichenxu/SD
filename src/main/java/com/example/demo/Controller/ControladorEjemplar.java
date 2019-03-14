6e277a938478fd488868e039fbaaa86e05dcb4a4
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-03-14 10:11:05 +0100

    pull

--- src/main/java/com/example/demo/Controller/ControladorEjemplar.java
+++ src/main/java/com/example/demo/Controller/ControladorEjemplar.java
@@ -1,12 +1,18 @@
 package com.example.demo.Controller;
 
 
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Model.Ejemplar;
+import com.example.demo.Model.Moneda;
+import com.example.demo.Model.Proveedor;
 import com.example.demo.Repository.RepositorioEjemplar;
 
 @Controller
@@ -15,13 +21,21 @@ public class ControladorEjemplar {
 	private RepositorioEjemplar repE;
 	
 	@RequestMapping(value="/ejemplarAniadir")
-	public String ejemplarAniadir(Model model,Ejemplar ejemplar) {
+	public String ejemplarAniadir(Model model, Ejemplar ejemplar) {
 		repE.save(ejemplar);
 		return "index";
 	}
-	@RequestMapping(value="/ejemplarConsultar")
-	public String ejemplaConsultar(Model model,Ejemplar ejemplar) {
-		repE.save(ejemplar);
-		return "index";
+
+	@RequestMapping(value = "/ejemplarConsultar")
+	public String ejemplaConsultar(Model model, Ejemplar ejemplar) {
+		List<Ejemplar> e = repE.findByModeloAndAnioAndCiudadAndFechaAdquisicionAndConservacionAndProveedorAndMoneda(ejemplar.getModelo(),
+				ejemplar.getAnio(),ejemplar.getCiudad(),
+				ejemplar.getFechaAdquisicion(),ejemplar.getConservacion(),
+				ejemplar.getProveedor(),
+				ejemplar.getMoneda());
+		if(!e.isEmpty()) {
+			model.addAttribute("ejemplar",e.get(0));
+		}
+		return "";
 	}
 }
--- src/main/java/com/example/demo/Repository/RepositorioEjemplar.java
+++ src/main/java/com/example/demo/Repository/RepositorioEjemplar.java
@@ -1,7 +1,18 @@
 package com.example.demo.Repository;
 
 import com.example.demo.Model.Ejemplar;
+import com.example.demo.Model.Moneda;
+import com.example.demo.Model.Proveedor;
+
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.OneToOne;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepositorioEjemplar extends JpaRepository<Ejemplar,Long> {
+	List<Ejemplar> findByModeloAndAnioAndCiudadAndFechaAdquisicionAndConservacionAndProveedorAndMoneda(String modelo,Date anio,String ciudad,
+			Date fechaAdquisicion,String conservacion,Proveedor proveedor,
+			Moneda moneda);
 }
