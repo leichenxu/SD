6e5324a0bf5082ba5f71fa747e66b55fd76c74f3
ziyao <ziyaozhan1998@gmail.com>
2019-03-21 14:41:12 +0100

    html

--- src/main/java/com/example/demo/Controller/ControladorMoneda.java
+++ src/main/java/com/example/demo/Controller/ControladorMoneda.java
@@ -1,8 +1,11 @@
 package com.example.demo.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Model.Moneda;
@@ -14,9 +17,16 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@RequestMapping("moneda")
 public class ControladorMoneda {
     @Autowired
     private RepositorioMoneda repM;
+
+    @PostMapping
+    public ResponseEntity<?> addMoneda (@ModelAttribute Moneda moneda){
+        this.repM.save(moneda);
+        return ResponseEntity.noContent().build();
+    }
 
     @RequestMapping(value = "/monedaAniadir")
     public String monedaAniadir(Model model, Moneda moneda) {
