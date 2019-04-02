cb9952c8fb7a6635c44321b47c6aee1af8ba60f9
ziyao <ziyaozhan1998@gmail.com>
2019-04-02 17:06:59 +0200

    provedor add

--- src/main/java/com/example/demo/Controller/ControladorProveedor.java
+++ src/main/java/com/example/demo/Controller/ControladorProveedor.java
@@ -1,17 +1,16 @@
 package com.example.demo.Controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.example.demo.Model.Ejemplar;
+import com.example.demo.Model.Moneda;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.Model.Proveedor;
 import com.example.demo.Repository.RepositorioProveedor;
@@ -28,9 +27,27 @@ public class ControladorProveedor {
 		model.addAttribute("Ejemplar",ejemplar);
 		return "ProveedorDeUnEjemplar";
 	}
-	@RequestMapping(value = "/proveedorAniadir")
-	public String proveedor(Model model, Proveedor proveedor) {
+	@RequestMapping(value = "/proveedorAniadir",method = RequestMethod.POST)
+	public String proveedor(@RequestParam("codigoIdentificacionFiscal") String codigoIdentificacionFiscal,
+							@RequestParam("nombre") String nombre,
+							@RequestParam("codigoPostal") String codigoPostal,
+							@RequestParam("email") String email,
+							@RequestParam("telefono") String telefono,
+							Model model) {
+		Proveedor proveedor = new Proveedor(codigoIdentificacionFiscal,nombre,codigoPostal,email,telefono);
 		repP.save(proveedor);
+		return "index";
+	}
+
+	@RequestMapping(value = "/monedaAniadir",method= RequestMethod.POST)
+	public String monedaAniadir(@RequestParam("valorFacial") String valorFacial,
+								@RequestParam("unidadMonetaria") String unidadMonetaria,
+								@RequestParam("diametro") String diametro,@RequestParam("peso") String peso,
+								@RequestParam("metales") String metales,@RequestParam("descripcion") String descripcion
+			,Model model) {
+		Moneda moneda=new Moneda(Double.valueOf(valorFacial),unidadMonetaria,Float.valueOf(diametro),
+				Float.valueOf(peso), Arrays.asList(metales.split(",")),descripcion);
+
 		return "index";
 	}
 
